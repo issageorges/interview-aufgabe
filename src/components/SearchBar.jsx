7f182bbc765c7f50ef982d008b41ad1ab34d3290
issa georges <issageorges.qi@gmail.com>
2024-06-18 12:30:24 +0200

    fixed searchbar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -8,7 +8,7 @@ export default function SearchBar({query,setQuery}) {
        <input type="text"
         placeholder="Search by color..."
         value={query}
-        onChange={e => setQuery(e.target.value)}
+        onChange={(e) => {setQuery(e.target.value)}}
         className="border border-black focus:outline-none rounded-2xl  m-3 py-1 px-2"/>
 
       </div>
